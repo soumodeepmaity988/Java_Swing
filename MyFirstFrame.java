b041aea2e2aea97678332374f496e2b48e4e4eca
soumodeepmaity988 <74635901+soumodeepmaity988@users.noreply.github.com>
2021-01-30 13:22:50 +0530

    MyFirstFrame in java Swing program

---
+++ MyFirstFrame.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import javax.swing.*;
+
+class MyFirstFrame{
+    public static void main(String[]args){
+       JFrame frame=new JFrame();
+       frame.setVisible(true);
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
