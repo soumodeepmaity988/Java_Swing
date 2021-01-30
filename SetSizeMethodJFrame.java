f540788fc3a0b2dae062cfdf2f0bc77e45d4b8ce
soumodeepmaity988 <74635901+soumodeepmaity988@users.noreply.github.com>
2021-01-30 13:32:23 +0530

    Setting frame hight and width in JFrame

---
+++ SetSizeMethodJFrame.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+class SetSizeMethodJFrame{
+    public static void main(String[] args){
+        JFrame frame=new JFrame();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(700,300);
+
+    }
+}
