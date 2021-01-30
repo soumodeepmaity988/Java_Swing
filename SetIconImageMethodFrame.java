061f817ce361475b8f6a96b549e678382a450857
soumodeepmaity988 <74635901+soumodeepmaity988@users.noreply.github.com>
2021-01-30 15:08:52 +0530

    setIconImage to set icon of the frame in JFrame

---
+++ SetIconImageMethodFrame.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+class SetIconImageMethodJFrame{
+    public static void main(String[] args){
+        JFrame frame=new JFrame();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(100,100,700,700);
+        frame.setTitle("My Frame");
+        Image icon = Toolkit.getDefaultToolkit().getImage("E:\\programming\\Java_Swing\\src\\com\\company\\instagram.png");
+        frame.setIconImage(icon);
+    }
+}
