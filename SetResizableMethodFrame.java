f41abbb443efaff10eb570aee2259b0548f722af
soumodeepmaity988 <74635901+soumodeepmaity988@users.noreply.github.com>
2021-01-30 15:28:35 +0530

    setResizable method for seting default size  of the frame in JFrame

---
+++ SetResizableMethodFrame.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+class SetResizableMethodJFrame{
+    public static void main(String[] args){
+        JFrame frame=new JFrame();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(100,100,700,700);
+        frame.setTitle("My Frame");
+        Image icon = Toolkit.getDefaultToolkit().getImage("E:\\programming\\Java_Swing\\src\\com\\company\\instagram.png");
+        frame.setIconImage(icon);
+        Container c=frame.getContentPane();
+        c.setBackground(Color.red);
+        frame.setResizable(false);
+    }
+}
