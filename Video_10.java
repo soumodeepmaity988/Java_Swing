1be7475f07b308bb6cea9e6c41dc098c59f8d588
soumodeepmaity988 <74635901+soumodeepmaity988@users.noreply.github.com>
2021-03-16 07:55:25 +0530

    JButton wity anonymous inner class

---
+++ Video_10.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLOutput;
+
+class ActionDemo2{
+    public static void main(String[]args){
+        JFrame frame=new JFrame();
+        frame.setBounds(100,100,1000,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        Container c=frame.getContentPane();
+        c.setLayout(null);
+
+        JButton btn=new JButton("Click me");
+        btn.setBounds(100,100,100,50);
+        c.add(btn);
+
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                c.setBackground(Color.red);
+                btn.setBackground(Color.YELLOW);
+                System.out.println("Button is Clicked");
+            }
+        });
+    }
+}
